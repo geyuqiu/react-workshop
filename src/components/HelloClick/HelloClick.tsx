0b3c004a517bc9360d381dd39461492a0745d2de
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-01-27 15:20:22 +0100

    wording

--- src/components/HelloClick/HelloClick.tsx
+++ src/components/HelloClick/HelloClick.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
 export default function HelloClick () {
-  const [buttonWasClicked, setButtonWasClicked] = useState<boolean>(false);
+  const [buttonWasClicked, setButtonWasClicked]
+    = useState<boolean>(false);
 
   return (
     <>
       <button className="bg-blue-100 border"
         onClick={() => setButtonWasClicked(!buttonWasClicked)}
       >
-        say hello!
+        say hello
       </button>
       <p>
-        {buttonWasClicked ? 'Hello button' : 'Hello World'}
+        {buttonWasClicked ? 'Hello button!' : 'Hello World!'}
       </p>
     </>
   );
