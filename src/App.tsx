65c59fc4ff565cb9b6292d932a8c43ccc6450cc0
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-01-27 14:47:44 +0100

    for loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Hello from './components/Hello/Hello';
+import HelloList from './components/HelloList/HelloList';
 
 function App() {
   return(
     <>
       <Hello name="Felix"/>
-      <Hello />
-      <Hello />
-      <Hello />
+      <HelloList />
     </>
   )
 }
--- src/components/HelloList/HelloList.tsx
+++ src/components/HelloList/HelloList.tsx
@@ -1,4 +1,16 @@
 import React from "react";
 export default function HelloList () {
-  return <p >Hello Robert</p>;
+  const names: string[] = ["A", "B", "C"];
+  // names.push('D') // valid
+  // names[0] = ('D') // valid
+
+  return (
+    <ul>
+      {
+        names.map((name, i) =>
+          <li key={i}>Hello {name}</li>
+        )
+      }
+    </ul>
+  );
 }
