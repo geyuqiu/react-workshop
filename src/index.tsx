c4e9cb41cf1bc2481116818f60c8e88127b4ecd2
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-10-20 11:09:39 +0200

    usestate

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createRoot } from 'react-dom/client';
 
 const rootElement = document.getElementById('root') as HTMLElement;
