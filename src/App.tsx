e61caa6ff1d092a7d31fb6b43006a3c38d8fa439
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-10-20 11:38:06 +0200

    test name input with input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Hello from './components/Hello/Hello';
 import HelloList from './components/HelloList/HelloList';
 import HelloClick from './components/HelloClick/HelloClick';
+import NameInput from "./components/NameInput/NameInput";
 
 function App() {
-  return(
-    <>
-      <Hello name="Felix"/>
-      <Hello/>
-      <HelloList />
-      <HelloClick />
-    </>
-  )
+    const [name, setName]
+        = useState<string>('');
+
+    return (
+        <>
+            <Hello name="Felix"/>
+            <Hello/>
+            <HelloList/>
+            <HelloClick/>
+            <NameInput name={name} setName={setName}/>
+        </>
+    )
 }
 
 export default App;
